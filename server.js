8745714698518f7a97fe0fe59ebfc827faa6c652
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-12-28 22:51:42 -0300

    gitignore

---
+++ server.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+import OpenAI from "openai";
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+
+    const openai = new OpenAI({
+        apiKey: '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',
+    });
+
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: "¿cuál es la película donde aparece un tiburon y tiene una canción iconica?" }],
+        model: "gpt-3.5-turbo",
+    });
+
+  return chatCompletion;
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
+
