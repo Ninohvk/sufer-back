25975dafb7f16fbedcbd106c209253fb03ac2a67
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-12-29 13:09:52 -0300

    test endpoint

--- server.js
+++ server.js
@@ -8,18 +8,19 @@ const fastify = Fastify({
 import OpenAI from "openai";
 
 // Declare a route
-fastify.get('/', async function handler (request, reply) {
+fastify.get('/movies', async function handler (request, reply) {
 
-    const openai = new OpenAI({
-        apiKey: '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',
-    });
+    // const openai = new OpenAI({
+    //     apiKey: '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',
+    // });
 
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: "¿cuál es la película donde aparece un tiburon y tiene una canción iconica?" }],
-        model: "gpt-3.5-turbo",
-    });
+    // const chatCompletion = await openai.chat.completions.create({
+    //     messages: [{ role: "user", content: "¿cuál es la película donde aparece un tiburon y tiene una canción iconica?" }],
+    //     model: "gpt-3.5-turbo",
+    // });
 
-  return chatCompletion;
+  // return chatCompletion;
+  return { movies: 'movies' };
 })
 
 // Run the server!
