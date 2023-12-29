3a2c6018583e17a2ed388901c5c5b923b676ed74
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-12-29 00:09:00 -0300

    port 1000

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ fastify.get('/', async function handler (request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 443 })
+  await fastify.listen({ port: 1000 })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
