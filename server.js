c18b30b0000af094ad94a2353e158a099480adeb
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-12-29 15:23:28 -0300

    dotenv y post movies

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-import 'dotenv/config'
+import path from 'path';
+import dotenv from 'dotenv';
+const dir = `./.env`;
+dotenv.config({path: path.resolve(process.env.NODE_ENV? `${dir}.${process.env.NODE_ENV}` : dir)});
+const ENV = process.env;
+
 // Import the framework and instantiate it
 import Fastify from 'fastify'
 const fastify = Fastify({
@@ -14,9 +19,8 @@ fastify.get('/', async function handler (request, reply) {
 
 // Declare a route
 fastify.get('/movies', async function handler (request, reply) {
-    console.log('/movies', process.env.OPENAI_API_KEY);
     const openai = new OpenAI({
-        apiKey: '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',
+        apiKey: ENV.OPENAI_API_KEY,
     });
 
     const chatCompletion = await openai.chat.completions.create({
@@ -25,6 +29,21 @@ fastify.get('/movies', async function handler (request, reply) {
     });
 
   return chatCompletion;
+})
+
+// Declare a route
+fastify.post('/movies', async function handler (request, reply) {
+  // const openai = new OpenAI({
+  //     apiKey: ENV.OPENAI_API_KEY,
+  // });
+
+  // const chatCompletion = await openai.chat.completions.create({
+  //     messages: [{ role: "user", content: "¿cuál es la película donde aparece un tiburon y tiene una canción iconica?" }],
+  //     model: "gpt-3.5-turbo",
+  // });
+  console.log('request', request)
+  console.log('reply', reply)
+  return request.body | '/movies';
 })
 
 // Run the server!
